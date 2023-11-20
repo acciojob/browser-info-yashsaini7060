cbd3ae22d72413adcadbe26d46f263de86da9577
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-21 00:28:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const agent = window.navigator.userAgent:
+const name = window.navigator.appName;
+const version = window.navigator.appVersion;
+document.body.innerText="agent";
