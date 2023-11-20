eaef832810091c467a03d06cd24924f72b49e9c1
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-21 01:08:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-//your JS code here. If required.
-const agent = window.navigator.userAgent:
+const agent = window.navigator.userAgent;
 const name = window.navigator.appName;
 const version = window.navigator.appVersion;
-document.body.innerText="agent";
+// document.body.innerText="testinggg";
+const str = "You are using " + name + version + "version";
+const divTag = document.createElement("div");
+divTag.id="browser-info";
+divTag.innerText=str;
+document.body.appendChild(divTag)
